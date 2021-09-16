14b08860871f4653762752f222d11d285d75bc5a
yoyomanhihi <Supermillie201>
2021-09-16 19:00:11 +0200

    v2.3.1: MLP for segmentation. Simple SGD with only full tumor or full non_tumor: [Not working yet] Slice the 2d model in a 2d image in order to segment the tumor.

---
+++ medical_FL_utils.py
@@ -0,0 +1,226 @@
+import pickle
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Activation
+import tensorflow as tf
+from sklearn.metrics import accuracy_score
+from tensorflow.keras.optimizers import SGD
+from sklearn.preprocessing import LabelBinarizer
+import sys
+import cv2
+
+np.set_printoptions(threshold=sys.maxsize)
+
+
+def load(path):
+    ''' Load the pickle data. The data already contains the 32x32 (32x32x32 in case of 3d) and a correct binary classification'''
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+        return data
+
+
+def prepareTrainTest(path):
+    ''' Open the data and prepare it for ML
+        args:
+            path: path to the file where the data is stored
+        return:
+            x_train: training images
+            y_train: training labels
+            x_test: test images
+            y_test: test labels'''
+    data = load(path)
+    data = np.array(data)
+    np.random.shuffle(data)
+    size = len(data)
+    train_size = int(0.8*size)
+    inputs = []
+    outputs = []
+    for elem in data:
+        inputs.append(elem[0]/255) #TOFIX: also divide x, y by 255
+        outputs.append(elem[1])
+    x_train = inputs[:train_size]
+    y_train = outputs[:train_size]
+    x_test = inputs[train_size:]
+    y_test = outputs[train_size:]
+    y_train = np.asarray(y_train).astype('float32').reshape((-1, 1))
+    y_test = np.asarray(y_test).astype('float32').reshape((-1, 1))
+    return x_train, y_train, x_test, y_test
+
+
+class SimpleMLP:
+    ''' Build and return a simple MLP model'''
+    @staticmethod
+    def build(shape, classes):
+        model = Sequential()
+        model.add(Dense(200, input_shape=(shape,)))
+        model.add(Activation("relu"))
+        model.add(Dense(200))
+        model.add(Activation("relu"))
+        model.add(Dense(classes))
+        model.add(Activation("sigmoid"))
+        return model
+
+
+def bcr(TP, FP, TN, FN):
+    ''' Compute the bcr score of the model
+        args:
+            TP: true positives
+            FP: false positives
+            TN: true negatives
+            FN: false negatives'''
+    left = 0
+    if TP+FN > 0:
+        left = TP / (TP + FN)
+    right = 0
+    if FP + TN > 0:
+        right = TN / (FP + TN)
+    return (left + right) / 2
+
+
+def test_model(x_test, y_test, model):
+    """ Calculates the accuracy and the loss of the model
+        args:
+            x_test: test set data
+            y_test: test set labels
+            model: the model
+        return:
+            acc: accuracy of the model
+
+    """
+    test_batched = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(len(y_test))
+    for (X_test, Y_test) in test_batched:
+        print("shape: " + str(np.shape(X_test)))
+        predictions = model.predict(X_test)
+    print(predictions)
+    print(y_test)
+    for i in range(len(predictions)):
+        if predictions[i] > 0.5:
+            predictions[i] = 1
+        else:
+            predictions[i] = 0
+    TP = 0
+    TN = 0
+    FP = 0
+    FN = 0
+    for k in range(len(predictions)):
+        if predictions[k] == 0:
+            if y_test[k] == 0:
+                TN += 1
+            else:
+                FN += 1
+        if predictions[k] == 1:
+            if y_test[k] == 1:
+                TP += 1
+            else:
+                FP += 1
+    acc = bcr(TP, FP, TN, FN)
+    print("Finale accuracy = " + str(acc))
+    return acc
+
+
+def simpleSGD_2d(x_train, y_train, x_test, y_test, lr = 0.01, comms_round = 100):
+    ''' Simple SGD algorithm for 32x32 images
+        args:
+            x_train: training images
+            y_train: training labels
+            x_test: test images
+            y_test: test labels
+            lr: learning rate
+            comms_round: number of communication rounds
+        returns:
+            SGD_model: model fitted
+    '''
+
+    loss = 'binary_crossentropy'
+    metrics = ['accuracy']
+    optimizer = SGD(lr=lr,
+                    decay=lr / comms_round,
+                    momentum=0.9
+                    )
+
+    SGD_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(len(y_train)).batch(320)
+    smlp_SGD = SimpleMLP()
+    SGD_model = smlp_SGD.build(1026, 1)
+
+    SGD_model.compile(loss=loss,
+                  optimizer=optimizer,
+                  metrics=metrics)
+
+    # fit the SGD training data to model
+    _ = SGD_model.fit(SGD_dataset, epochs=100, verbose=2)
+
+    #test the SGD global model and print out metrics
+
+    SGD_acc = test_model(x_test, y_test, SGD_model)
+
+    return SGD_model
+
+
+def simpleSGD_3d(X_train, y_train, X_test, y_test, lr = 0.01, comms_round = 100):
+    ''' Simple SGD algorithm for 32x32x32 images
+        args:
+            x_train: training images
+            y_train: training labels
+            x_test: test images
+            y_test: test labels
+            lr: learning rate
+            comms_round: number of communication rounds
+        returns:
+            SGD_model: model fitted
+    '''
+
+    loss = 'binary_crossentropy'
+    metrics = ['accuracy']
+    optimizer = SGD(lr=lr,
+                    decay=lr / comms_round,
+                    momentum=0.9
+                    )
+
+    SGD_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train)).shuffle(len(y_train)).batch(320)
+    smlp_SGD = SimpleMLP()
+    SGD_model = smlp_SGD.build(32771, 1)
+
+    SGD_model.compile(loss=loss,
+                  optimizer=optimizer,
+                  metrics=metrics)
+
+    # fit the SGD training data to model
+    _ = SGD_model.fit(SGD_dataset, epochs=100, verbose=2)
+
+    SGD_acc = test_model(X_test, y_test, SGD_model)
+
+    return SGD_model
+
+
+def crop_2d(image, y, x): #vertical, horizontal
+    ''' Return a 32x32 image with top left corner of coordonate(y, x) '''
+    crop_img = image[y:y + 32, x:x + 32]
+    return crop_img
+
+
+def segmentation_2d(model, image_path):
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    print("shape image: " + str(image.shape))
+    predictions = np.zeros((512, 512))
+    image = image/255
+    for y in range(0, 480, 10):
+        print("predicting line: " + str(y))
+        for x in range(0, 480, 10):
+            flatten_subimage = crop_2d(image, y, x).flatten()
+            flatten_subimage = np.append(flatten_subimage, y)
+            flatten_subimage = np.append(flatten_subimage, x)
+            reshaped = np.reshape(flatten_subimage, (1,1026))
+            print(reshaped)
+            pred = model.predict(reshaped)
+            # print(pred[0])
+            if pred > 0.5:
+                print("FOUND A TUMOR HERE GUYS")
+                predictions[y:y + 32, x:x + 32] += 1
+    print(predictions)
+    return predictions
+
+
+
+
--- medical_SGD.py
+++ medical_SGD.py
@@ -1,7 +1,20 @@
 import pickle
 import numpy as np
-import medical_FL_utils_2d as med_utils_2d
-import medical_FL_utils_3d as med_utils_3d
+import medical_FL_utils as med_utils
+from tensorflow import keras
+import tensorflow as tf
 
-x_train, y_train, x_test, y_test = med_utils_3d.prepareTrainTest('medium_3d_dataset.pickle')
-med_utils_3d.simpleSGD(x_train, y_train, x_test, y_test)
+image_path = 'NSCLC2 - Lung_Cancers3/manifest-1603198545583/NSCLC-Radiomics/LUNG1-301/images/image_68.png'
+
+x_train, y_train, x_test, y_test = med_utils.prepareTrainTest('small_dataset.pickle')
+
+# model = med_utils.simpleSGD_2d(x_train, y_train, x_test, y_test)
+#
+# model.save('small_model.h5')
+
+model = keras.models.load_model('small_model.h5')
+
+test_batched = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(len(y_test))
+SGD_acc = med_utils.test_model(x_test, y_test, model)
+
+med_utils.segmentation_2d(model, image_path)
