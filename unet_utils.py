c434907c97c5036a3a691ebc48e13f69fc2619e3
yoyomanhihi <Supermillie201>
2021-11-01 11:16:04 +0100

    v4.2.2: U-net architecture New U-Net architecture working on tumors but not generalizing (overfitting ?). Some clean up

--- unet_utils.py
+++ unet_utils.py
@@ -38,8 +38,10 @@ def dice_coef(y_true, y_pred):
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
+
 def dice_coef_loss(y_true, y_pred):
     return -dice_coef(y_true, y_pred)
+
 
 # Dice Coefficient to work outside Tensorflow
 def dice_coef_2(y_true, y_pred):
@@ -55,7 +57,6 @@ def load(path):
     with open(path, 'rb') as f:
         data = pickle.load(f)
         return data
-
 
 
 def prepareTrainTest(dataset_path):
@@ -74,129 +75,6 @@ def prepareTrainTest(dataset_path):
     x_test = inputs[train_size:]
     y_test = outputs[train_size:]
     return x_train, y_train, x_test, y_test
-
-
-
-class Unet():
-    def __init__(self):
-        print("Initial U-Net model...")
-        self.model = self.initial_model()
-
-    def initial_model(self):
-        concat_axis = 3
-
-        inputs = Input((512, 512, 1))
-        conv1_1 = Conv2D(64, (3, 3), activation='relu', padding='valid', name='conv1_1')(inputs)
-        conv1_2 = Conv2D(64, (3, 3), activation='relu', padding='valid', name='conv1_2')(conv1_1)
-        pool1 = MaxPooling2D(pool_size=(2, 2), name='maxpooling_1')(conv1_2)
-
-        conv2_1 = Conv2D(128, (3, 3), activation='relu', padding='valid', name='conv2_1')(pool1)
-        conv2_2 = Conv2D(128, (3, 3), activation='relu', padding='valid', name='conv2_2')(conv2_1)
-        pool2 = MaxPooling2D(pool_size=(2, 2), name='maxpooling_2')(conv2_2)
-
-        conv3_1 = Conv2D(256, (3, 3), activation='relu', padding='valid', name='conv3_1')(pool2)
-        conv3_2 = Conv2D(256, (3, 3), activation='relu', padding='valid', name='conv3_2')(conv3_1)
-        pool3 = MaxPooling2D(pool_size=(2, 2), name='maxpooling_3')(conv3_2)
-
-        conv4_1 = Conv2D(512, (3, 3), activation='relu', padding='valid', name='conv4_1')(pool3)
-        conv4_2 = Conv2D(512, (3, 3), activation='relu', padding='valid', name='conv4_2')(conv4_1)
-        pool4 = MaxPooling2D(pool_size=(2, 2), name='maxpooling_4')(conv4_2)
-
-        conv5_1 = Conv2D(1024, (3, 3), activation='relu', padding='valid', name='conv5_1')(pool4)
-        conv5_2 = Conv2D(1024, (3, 3), activation='relu', padding='valid', name='conv5_2')(conv5_1)
-
-        upsampling1 = Conv2DTranspose(512, (2, 2), strides=(2, 2), padding='valid', name='upsampling1')(conv5_2)
-        crop_conv4_2 = Cropping2D(cropping=((4, 4), (4, 4)), name='cropped_conv4_2')(conv4_2)
-        up6 = concatenate([upsampling1, crop_conv4_2], axis=concat_axis, name='skip_connection1')
-        conv6_1 = Conv2D(512, (3, 3), activation='relu', padding='valid', name='conv6_1')(up6)
-        conv6_2 = Conv2D(512, (3, 3), activation='relu', padding='valid', name='conv6_2')(conv6_1)
-
-        upsampling2 = Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='valid', name='upsampling2')(conv6_2)
-        crop_conv3_2 = Cropping2D(cropping=((16, 16), (16, 16)), name='cropped_conv3_2')(conv3_2)
-        up7 = concatenate([upsampling2, crop_conv3_2], axis=concat_axis, name='skip_connection2')
-        conv7_1 = Conv2D(256, (3, 3), activation='relu', padding='valid', name='conv7_1')(up7)
-        conv7_2 = Conv2D(256, (3, 3), activation='relu', padding='valid', name='conv7_2')(conv7_1)
-
-        upsampling3 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='valid', name='upsampling3')(conv7_2)
-        crop_conv2_2 = Cropping2D(cropping=((40, 40), (40, 40)), name='cropped_conv2_2')(conv2_2)
-        up8 = concatenate([upsampling3, crop_conv2_2], axis=concat_axis, name='skip_connection3')
-        conv8_1 = Conv2D(128, (3, 3), activation='relu', padding='valid', name='conv8_1')(up8)
-        conv8_2 = Conv2D(128, (3, 3), activation='relu', padding='valid', name='conv8_2')(conv8_1)
-
-        upsampling4 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='valid', name='upsampling4')(conv8_2)
-        crop_conv1_2 = Cropping2D(cropping=((88, 88), (88, 88)), name='cropped_conv1_2')(conv1_2)
-        up9 = concatenate([upsampling4, crop_conv1_2], axis=concat_axis, name='skip_connection4')
-        conv9_1 = Conv2D(64, (3, 3), activation='relu', padding='valid', name='conv9_1')(up9)
-        conv9_2 = Conv2D(64, (3, 3), activation='relu', padding='valid', name='conv9_2')(conv9_1)
-
-        conv10 = Conv2D(1, (1, 1), activation='sigmoid', name='conv10')(conv9_2)
-
-        model = Model(inputs=[inputs], outputs=[conv10])
-        return model
-
-    def model_summary(self):
-        self.model.summary()
-
-    def get_model(self):
-        return self.model
-
-
-def get_model(img_size, num_classes):
-    inputs = keras.Input(shape=img_size + (1,))
-
-    ### [First half of the network: downsampling inputs] ###
-
-    # Entry block
-    x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-
-    previous_block_activation = x  # Set aside residual
-
-    # Blocks 1, 2, 3 are identical apart from the feature depth.
-    for filters in [64, 128, 256]:
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
-
-        # Project residual
-        residual = layers.Conv2D(filters, 1, strides=2, padding="same")(
-            previous_block_activation
-        )
-        x = layers.add([x, residual])  # Add back residual
-        previous_block_activation = x  # Set aside next residual
-
-    ### [Second half of the network: upsampling inputs] ###
-
-    for filters in [256, 128, 64, 32]:
-        x = layers.Activation("relu")(x)
-        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.Activation("relu")(x)
-        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.UpSampling2D(2)(x)
-
-        # Project residual
-        residual = layers.UpSampling2D(2)(previous_block_activation)
-        residual = layers.Conv2D(filters, 1, padding="same")(residual)
-        x = layers.add([x, residual])  # Add back residual
-        previous_block_activation = x  # Set aside next residual
-
-    # Add a per-pixel classification layer
-    outputs = layers.Conv2D(num_classes, 1, activation="sigmoid", padding="same")(x)
-
-    # Define the model
-    model = keras.Model(inputs, outputs)
-    return model
 
 
 def get_model2(optimizer, loss_metric, metrics, lr=1e-3):
@@ -249,34 +127,6 @@ def get_model2(optimizer, loss_metric, metrics, lr=1e-3):
     return model
 
 
-
-def test_model(x_test, y_test, model):
-    """ Calculates the accuracy and the loss of the model
-        args:
-            x_test: test set data
-            y_test: test set labels
-            model: the model
-        return:
-            acc: accuracy of the model
-
-    """
-    dices = []
-    test_batched = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(len(y_test))
-    for (X_test, Y_test) in test_batched:
-        print("shape: " + str(np.shape(X_test)))
-        predictions = model.predict(X_test)
-    predictions[predictions >= 0.5] = 1
-    predictions[predictions <= 0.5] = 1
-    for i in range(len(predictions)):
-        dice = dice_coef(y_test[i], predictions[i])
-        dices.append(dice)
-    dices = np.array(dices)
-    mean_dice = np.mean(dices)
-    print("Finale accuracy = " + str(mean_dice))
-    return mean_dice
-
-
-
 def simpleSGD_2d(x_train, y_train, x_test, y_test):
     ''' Simple SGD algorithm for 32x32 images
         args:
@@ -311,8 +161,6 @@ def simpleSGD_2d(x_train, y_train, x_test, y_test):
     # Train the model, doing validation at the end of each epoch.
     epochs = 50 #CHECK
     model.fit(x_train, y_train, batch_size=16, epochs=epochs) # CHECK callbacks
-
-    # test_model(x_test, y_test, model)
 
 
     return model
@@ -406,8 +254,5 @@ def segmentation_2d(model, client_path, img_nbr, zone):
     mask[mask > 210] = 1  # Set out of tumor to 1
     dice = dice_coef_2(mask, predictions)
     print("dice accuracy: " + str(dice))
-    # print("Final dice accuracy = " + str(dice))
-
-    # np.save("predictions " + str(img_nbr), predictions)
 
     return cntr, predictions
