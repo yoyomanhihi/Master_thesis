7c70cbca2a563e4e858558d54ae09754b125d370
yoyomanhihi <thibaud.misonne@hotmail.com>
2022-02-10 18:31:07 +0100

    meddataaugm

--- submit.sh
+++ submit.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #
 #SBATCH --job-name=name
-#SBATCH --output="nodataaugm.txt" 
+#SBATCH --output="meddataaugm.txt"
 #
+#SBATCH -w, --nodelist=mb-cas001
 #SBATCH --ntasks=1
 #SBATCH --time=36:00:00
 #SBATCH --mem-per-cpu=8192
@@ -12,11 +13,11 @@
 module load releases/2019b
 module load TensorFlow/2.3.1-fosscuda-2019b-Python-3.7.4
 
-srun pip3 install --user --upgrade pip 
+srun pip3 install --user --upgrade pip
 srun pip3 install --user scikit-build
-srun pip3 install --user cmake 
+srun pip3 install --user cmake
 srun pip3 install --user -r requirements.txt
 srun pip3 install --user tensorflow
 srun nvidia-smi
 srun python3 -c 'from tensorflow.python.client import device_lib;print(device_lib.list_local_devices())'
-srun python3 main.py nodataaugm
+srun python3 main.py meddataaugm
