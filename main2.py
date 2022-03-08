92a1281fd952d70e77d9053cec8b56ac3b5ad357
yoyomanhihi <thibaud.misonne@hotmail.com>
2022-03-08 19:16:58 +0100

    v6.5.5 Plot x_axis + 1

--- main2.py
+++ main2.py
@@ -10,7 +10,7 @@ def main():
 
     try:
         # results = med_prep_2d.generateAndStore('2d_dataset_1.pickle', nbclients=300)
-        results = unet_running.build_and_save(datasetpath='datasets/dataset_heart_fedAvg/0', epochs=100, name=name)
+        results = unet_running.build_and_save(datasetpath='datasets/dataset_heart_fedAvg/0', epochs=100, name=name, preloaded='models/ds0_heart_20epochs(2).h5')
         # string = ("accuracy with lr: " + str(results))
 
         # print(string)
@@ -20,5 +20,5 @@ def main():
         mailSender.sendResults(True, None)
 
 # main()
-# unet_running.build_and_save_fedavg(datasetpath='datasets/dataset_heart_fedAvg', nbclients=3, name=name)
-unet_running.build_and_save(datasetpath='datasets/dataset_heart_fedAvg/1', epochs=100, name=name, preloaded = 'models/ds1_heart_16epochs(2).h5')
+# unet_running.build_and_save_fedavg(datasetpath='datasets/dataset_heart_fedAvg', preloaded='models/heart_fed_medbigda_27epochs(2).h5', nbclients=3, name=name)
+unet_running.build_and_save(datasetpath='datasets/dataset_heart_fedAvg/0', epochs=100, name=name, preloaded = 'models/ds0_heart_20epochs(2).h5')
--- plots.py
+++ plots.py
@@ -7,8 +7,10 @@ def history(train_accs, val_accs, name):
 
     plot_name = name + '.png'
 
-    plt.plot(train_accs)
-    plt.plot(val_accs)
+    x_axis = range(1, len(train_accs)+1)
+
+    plt.plot(train_accs, x_axis)
+    plt.plot(val_accs, x_axis)
     plt.title("Dice's coefficient by epoch")
     plt.ylabel("Dice's coefficient")
     plt.xlabel('epoch')
@@ -22,18 +24,20 @@ def history_fedavg(train_accs, val_accs, clientsnbr, name):
     plot_name = name + '.png'
     colors = ['blue', 'red', 'green']
 
+    x_axis = range(1, len(train_accs) + 1)
+
     for i in range(clientsnbr):
         train_accs_client = []
         for j in range(i, len(train_accs), clientsnbr):
             train_accs_client.append(train_accs[j])
         print(train_accs_client)
-        plt.plot(train_accs_client, color=colors[i])
+        plt.plot(train_accs_client, x_axis, color=colors[i])
     for i in range(clientsnbr):
         val_accs_client = []
         for j in range(i, len(val_accs), clientsnbr):
             val_accs_client.append(val_accs[j])
         print(val_accs_client)
-        plt.plot(val_accs_client, color=colors[i], linestyle='dotted')
+        plt.plot(val_accs_client, x_axis, color=colors[i], linestyle='dotted')
     plt.title("Dice's coefficient by epoch")
     plt.ylabel("Dice's coefficient")
     plt.xlabel('epoch')
@@ -82,12 +86,17 @@ def compare_fedAvg_to_separate_models(local_path, fed_path, client_nbr, nbclient
     local_train = local_lines[:local_mid]
     local_val = local_lines[local_mid:]
 
-    plt.plot(local_train, color='blue')
-    plt.plot(local_val, color='blue', linestyle='dotted')
+    x_axis1 = range(1, len(local_train) + 1)
+
+    plt.plot(local_train, x_axis1, color='blue')
+    plt.plot(local_val, x_axis1, color='blue', linestyle='dotted')
 
     fed_train, fed_val = interpret_fed_path(fed_path, client_nbr, nbclients)
-    plt.plot(fed_train, color='red')
-    plt.plot(fed_val, color='red', linestyle='dotted')
+
+    x_axis2 = range(1, len(fed_train) + 1)
+
+    plt.plot(fed_train, x_axis2, color='red')
+    plt.plot(fed_val, x_axis2, color='red', linestyle='dotted')
 
     plt.title("FedAvg vs local training for dataset " + str(client_nbr+1))
     plt.ylabel("Dice's coefficient")
